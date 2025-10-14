4bab02634c3da30dbdd0b7fc7fbae34c3712c11c
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2025-10-14 18:11:22 +0100

    dog type displayed, missing breed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
 import Masonry from "react-masonry-css";
+import dogsJson from "./dogs.json"
 import './App.css';
 
-const dogList = [
-  {id: 0, title: "Item One",},
-  {id: 1, title: "Item Two",},
-  {id: 2, title: "Item Three",},
-  {id: 3, title: "Item Four",},
-  {id: 4, title: "Item Five",},
-  {id: 4, title: "Item Six",},
-  {id: 4, title: "Item sEven",},
-]
+/*Converts json list into readable numbered object*/
+const dogs = [];
+Object.entries(dogsJson).forEach(([key, values]) => {
+  dogs.push(key)
+});
+
+console.log(dogs);
+
+
 const breakpointColumnsObj = {
   default: 5,
   1800: 5,
@@ -19,18 +20,16 @@ const breakpointColumnsObj = {
   500: 1
 };
 // Convert array to JSX items
-const dogView = dogList.map(function(dog) {
-  return <div className="masonry-item" key={dog.id}>
+const dogView = dogs.map(function(dog) {
+  return <div className="masonry-item">
   <div className="grid-item m-3">
     <div className="card bg-light text-dark">
       <div className="card-header"><strong>
-        {dog.title}
+        {dog}
       </strong></div>
-      <ul className="list-group list-group-flush">
+      {/*}<ul className="list-group list-group-flush">
         <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-      </ul>
+      </ul>*/}
     </div></div></div>
 });
 
